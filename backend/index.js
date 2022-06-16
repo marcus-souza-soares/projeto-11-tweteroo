d351d033aab4dc3b48a107a106861d6d17416bc3
Marcus <marcusadrenaline@gmail.com>
2022-06-16 20:13:17 -0300

    post tweet pronto

--- backend/index.js
+++ backend/index.js
@@ -5,19 +5,34 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-const usuarios = []
-
-server.get("/sign-up", (req, res) => {
-    res.send('oiii');
-});
+const usuarios = [];
+const tweets = [
+    {
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "eu amo o hub"
+    }
+];
 
 server.post("/sign-up", (req, res) => {
     usuarios.push(req.body);
-    res.send(usuarios);
+    res.send("OK");
 });
 
 server.get("/tweets", (req, res) => {
-    
+    res.send(tweets);
+});
+
+server.post("/tweets", (req, res) => {
+    const tweet = req.body;
+    usuarios.forEach(user => {
+        if(user.username === tweet.username){
+            tweets.push({
+                ...tweet,  avatar: user.avatar
+            })
+        }
+    })
+    res.send("OK");
 });
 
 server.listen(5000);
