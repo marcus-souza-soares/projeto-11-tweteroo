ba2f191624d20fe4af67ef41f79bf78a4dfb8001
Marcus <marcusadrenaline@gmail.com>
2022-06-16 19:12:35 -0300

    post no sing-up implementado

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,23 @@
 import express from 'express';
+import cors from 'cors';
 
 const server = express();
+server.use(express.json());
+server.use(cors());
+
+const usuarios = []
 
 server.get("/sign-up", (req, res) => {
     res.send('oiii');
-})
-server.listen(5000);
+});
 
+server.post("/sign-up", (req, res) => {
+    usuarios.push(req.body);
+    res.send(usuarios);
+});
+
+server.get("/tweets", (req, res) => {
+    
+});
+
+server.listen(5000);
